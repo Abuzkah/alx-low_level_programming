bc804bfdd31956385db792cd02dd26a360a20e88
Abubakar Mohamed <110993144+Abuzkah@users.noreply.github.com>
2022-09-14 13:38:34 -0700

    Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,24 +6,19 @@
  */
 void jack_bauer(void)
 {
-int a;
-int b;
+	int h, m;
 
-
-for (a = 0; a <= 23; a++)
-{
-for (b = 0; b <= 59; b++)
-{
-_putchar (a / 10 + '0');
-_putchar (a % 10 + '0');
-_putchar (a % 10 + '0');
-_putchar (b / 10 + '0');
-_putchar (b % 10 + '0');
-_putchar ('\n');
-
-}
-
-}
-
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
 }
 
