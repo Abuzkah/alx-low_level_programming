410da971a2abfc9e46b5262d458207d5b46d39d9
Abubakar Mohamed <110993144+Abuzkah@users.noreply.github.com>
2022-09-14 13:59:33 -0700

    The World looks like a multiplication-table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,22 +1,48 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * print_to_98 - print all natural numbers from n to 98.
- * @n: the number to start counting from to 98
- * Return: void returns nothing.
+ * print_times_table - Prints the n times table
+ * @n: number times table (0 < n <= 15)
+ * Return: no return
  */
-void print_to_98(int n)
+void print_times_table(int n)
 {
-	if (n < 98)
-	{
-		for (n = n; n < 98; n++)
-			printf("%d, ", n);
-		printf("%d\n", 98);
-	}
-	else
-	{
-		for (n = n; n > 98; n--)
-			printf("%d, ", n);
-		printf("%d\n", 98);
-	}
+int a, b, op;
+
+if (n >= 0 && n <= 15)
+{
+
+for (a = 0; a <= n; a++)
+{
+_putchar(48);
+for (b = 1; b <= n; b++)
+{
+op = a * b;
+_putchar(44);
+_putchar(32);
+if (op <= 9)
+
+{
+_putchar(32);
+_putchar(32);
+_putchar(op + 48);
 }
+
+else if (op <= 99)
+{
+_putchar(32);
+_putchar((op / 10) + 48);
+_putchar((op % 10) + 48);
+}
+
+else
+{
+_putchar(((op / 100) % 10) + 48);
+_putchar(((op / 10) % 10) + 48);
+_putchar((op % 10) + 48);
+}
+}
+_putchar('\n');
+}
+}
+}
+
