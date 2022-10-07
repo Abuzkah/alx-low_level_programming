cd044b844de545065ad03261340c4fe64f09c072
Abubakar Mohamed <110993144+Abuzkah@users.noreply.github.com>
2022-10-07 07:15:09 -0400

    Added 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup- copy a string.
+ * @str: string to be copied.
+ *
+ *
+ * Return: pointer to the copied string.
+ */
+char *_strdup(char *str)
+{
+	int strSize, i;
+	char *copyStr;
+
+	strSize = 0;
+	if (str == NULL)
+		return (NULL);
+	while (*(str + strSize))
+		strSize++;
+	strSize++; /* add 1 to print NULL*/
+	copyStr = malloc((strSize) * sizeof(char));
+	if (copyStr == NULL)
+		return (0);
+	for (i = 0; i < (strSize); i++)
+		*(copyStr + i) = *(str + i);
+	return (copyStr);
+}
