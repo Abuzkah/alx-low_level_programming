f5ea4e87fc87b435d8bc95cce31cc37e1977f112
Abubakar Mohamed <110993144+Abuzkah@users.noreply.github.com>
2022-10-07 07:54:17 -0400

    Fixed 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,18 +1,18 @@
-#include <stdio.h>
+#include "holberton.h"
 #include <stdlib.h>
+
 /**
- * malloc_checked - allocates memory using malloc.
- * @b: size of new memory array.
+ * malloc_checked - allocates memory using malloc
+ * @b: memory to allocate
  *
- *
- * Return: the string initialized.
+ * Return: pointer to allocated memory or normal process termination if error
  */
 void *malloc_checked(unsigned int b)
 {
-	void *memLoc;
+	void *p;
 
-	memLoc = malloc(b);
-	if (memLoc == NULL)
+	p = malloc(b);
+	if (p == NULL)
 		exit(98);
-	return (memLoc);
+	return (p);
 }
