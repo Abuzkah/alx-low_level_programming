45d505ea5e2a23ff33b4cf28dae2fdcc7fd2b81b
Abubakar Mohamed <110993144+Abuzkah@users.noreply.github.com>
2022-10-13 11:38:53 -0400

    fixed 0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef _function_like_macro_h_
-#define _function_like_macro_h_
+#ifndef _object_like_macro_h_
+#define _object_like_macro_h_
 
-#define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
+#define SIZE 1024
 
-#endif /* _function_like_macro_h_ */
+#endif /* _object_like_macro_h_ */
