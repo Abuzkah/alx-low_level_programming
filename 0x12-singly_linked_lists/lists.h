d1184b9c4b6cef9fa5d0b28209f77ba2bcc0afb6
Abubakar Mohamed <110993144+Abuzkah@users.noreply.github.com>
2023-01-31 19:43:24 -0500

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,8 @@
-#ifndef _LINKED_LIST
-#define _LINKED_LIST
+#ifndef LISTS
+#define LISTS
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 /**
  * struct list_s - singly linked list
@@ -17,11 +20,9 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-int _strlen(const char *s);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
 #endif
